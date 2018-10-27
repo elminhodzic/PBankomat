e0ebb30553cb69398712b8ccd7002c5dbb339344
Elmin Hodzic <elmin.hodzic@hotmail.com>
2018-10-27 23:47:48 +0200

    try catch

--- src/BankomatTest.java
+++ src/BankomatTest.java
@@ -1,8 +1,9 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BankomatTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
 		Scanner unos = new Scanner(System.in);
@@ -17,68 +18,78 @@ public class BankomatTest {
 			System.out.println("Unesite neku od ponudjenih opcija: \n");
 			System.out.println("1.Kreiraj racun \n2.Uplata na racun \n3.Transfer novca \n4.Stanje \n0.Prekid");
 
-			opcija = unos.nextInt();
+			try {
 
-			if (opcija == 1) {
+				opcija = unos.nextInt();
 
-				System.out.println("Unesite broj racuna: ");
-				int brojRacuna = unos.nextInt();
+				if (opcija == 1) {
+
+					System.out.println("Unesite broj racuna: ");
+					int brojRacuna = unos.nextInt();
+
+					unos.nextLine();
+					System.out.println("Unesite ime vlasnika: ");
+					String imeVlasnika = unos.nextLine();
+
+					System.out.println("Unesite stanje na racunu u KM-ovima: ");
+					double stanjeRacuna = unos.nextDouble();
+
+					d.kreirajRacun(brojRacuna, imeVlasnika, stanjeRacuna);
+					System.out.println();
+
+				}
+
+				if (opcija == 2) {
+
+					System.out.println("Unesite broj racuna: ");
+					int brojRacuna = unos.nextInt();
+
+					unos.nextLine();
+
+					System.out.println("Unesite iznos za uplatu u KM:");
+					double iznos = unos.nextDouble();
+
+					d.uplata(brojRacuna, iznos);
+					System.out.println();
+				}
+
+				if (opcija == 3) {
+
+					System.out.println("Unesite broj racuna sa kojeg prebacujete novac:");
+					int brojRacuna1 = unos.nextInt();
+
+					// unos.nextLine();
+					System.out.println("Unesite broj racuna na koji zelite prebaciti novac: ");
+					int brojRacuna2 = unos.nextInt();
+
+					// unos.nextLine();
+					System.out.println("unesite iznos:");
+					double iznos = unos.nextDouble();
+
+					d.transfer(brojRacuna1, brojRacuna2, iznos);
+					System.out.println();
+				}
+
+				if (opcija == 4) {
+
+					d.ispis();
+					System.out.println();
+				}
+
+				if (opcija == 0) {
+
+					System.out.println("hvala na usluzi: ");
+
+					System.exit(0);
+				}
+			} catch (InputMismatchException ex) {
+
+				System.out.println("Pogresan unos \n");
 
 				unos.nextLine();
-				System.out.println("Unesite ime vlasnika: ");
-				String imeVlasnika = unos.nextLine();
-
-				System.out.println("Unesite stanje na racunu u KM-ovima: ");
-				double stanjeRacuna = unos.nextDouble();
-
-				d.kreirajRacun(brojRacuna, imeVlasnika, stanjeRacuna);
-				System.out.println();
 
 			}
 
-			if (opcija == 2) {
-
-				System.out.println("Unesite broj racuna: ");
-				int brojRacuna = unos.nextInt();
-
-				unos.nextLine();
-
-				System.out.println("Unesite iznos za uplatu u KM:");
-				double iznos = unos.nextDouble();
-
-				d.uplata(brojRacuna, iznos);
-				System.out.println();
-			}
-
-			if (opcija == 3) {
-
-				System.out.println("Unesite broj racuna sa kojeg prebacujete novac:");
-				int brojRacuna1 = unos.nextInt();
-
-				//unos.nextLine();
-				System.out.println("Unesite broj racuna na koji zelite prebaciti novac: ");
-				int brojRacuna2 = unos.nextInt();
-
-				//unos.nextLine();
-				System.out.println("unesite iznos:");
-				double iznos = unos.nextDouble();
-
-				d.transfer(brojRacuna1, brojRacuna2, iznos);
-				System.out.println();
-			}
-
-			if (opcija == 4) {
-
-				d.ispis();
-				System.out.println();
-			}
-
-			if (opcija == 0) {
-
-				System.out.println("hvala na usluzi: ");
-
-				System.exit(0);
-			}
 		}
 		unos.close();
 	}
